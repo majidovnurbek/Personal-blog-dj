6642d1abe3328b6d337bb78b8f862a1dd808ebc8
Nurbek <152709750+majidovnurbek@users.noreply.github.com>
2025-05-04 22:50:17 +0500

    added article post codes

--- root/apps/forms.py
+++ root/apps/forms.py
@@ -8,10 +8,16 @@ from django import forms
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['first_name','last_name','username','email', 'password1', 'password2']
+        fields = ['first_name','last_name','username','email', 'password1', 'password2','is_staff']
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Username')
     password = forms.CharField(label='Password',widget=forms.PasswordInput)
-    
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'published_date']
--- root/apps/urls.py
+++ root/apps/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from .views import (
-    IndexView,
-    LoginView,
-    IndexView, RegisterView
-)
-
+    LoginView,RegisterView,home,post_detail,create_post,admin_dashboard,edit_post,delete_post)
 urlpatterns = [
-    path('pro',IndexView.as_view(), name='index'),
-    path('login',LoginView.as_view(), name='login'),
-    path('register',RegisterView.as_view(), name='register'),
+    path('',home, name='home'),
+    path('login/',LoginView.as_view(), name='login'),
+    path('register/',RegisterView.as_view(), name='register'),
+    path('post/<int:post_id>/', post_detail, name='post_detail'),
+    path('post/create',create_post, name='create_post'),
+    path('dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('post/edit/<int:pk>/', edit_post, name='edit_post'),
+    path('post/delete/<int:pk>/', delete_post, name='delete_post'),
+    
+
+
 ]
